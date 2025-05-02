933b3d1eafed8e797d8f72624514e3c58d73a8db
Zerox <zeroxarcam.com>
2025-05-02 12:09:27 -0500

    add script constants

---
+++ home360/src/app/shared/constants/messages.constants.ts
@@ -0,0 +1,8 @@
+export const FORM_MESSAGES = {
+  REQUIRED_FIELD: 'Por favor, completa todos los campos requeridos.',
+};
+
+export const CATEGORY_MESSAGES = {
+  CREATE_SUCCESS: 'Categoría creada exitosamente.',
+  CREATE_ERROR: 'Error al crear la categoría.',
+};
