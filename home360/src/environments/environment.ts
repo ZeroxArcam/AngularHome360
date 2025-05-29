f3d0d2ee48bf4fb7b3f23a492c0ab6158df14a0d
Zerox <zeroxarcam.com>
2025-05-28 22:01:18 -0500

    fix(environment): update production flag and users API URL for deployment

--- home360/src/environments/environment.ts
+++ home360/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
+  production: true,
   homeapiUrl: 'http://localhost:8081/api/v1',
-  usersapiUrl: 'http://localhost:8082/api/v1',
+  usersapiUrl: 'https://services-user.onrender.com/api/v1',
   visitapiUrl: 'http://localhost:8083/api/v1'
 };
